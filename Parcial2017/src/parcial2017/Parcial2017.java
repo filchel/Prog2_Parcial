0457b403ab3fea157ed1f3c95a91555eb5db5a6a
federico <federico@federico-MOV>
2017-05-06 13:37:41 -0300

    hola 2

--- Parcial2017/src/parcial2017/Parcial2017.java
+++ Parcial2017/src/parcial2017/Parcial2017.java
@@ -15,6 +15,6 @@ public class Parcial2017 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Puto el que lee");
     }
 }

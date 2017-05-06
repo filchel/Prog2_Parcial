6104d33a94572350854c53c70e956e1cbcf8ee99
federico <federico@federico-MOV>
2017-05-06 13:58:22 -0300

    ASFDA

---
+++ Parcial2017/src/parcial2017/Verificacion2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial2017;
+
+/**
+ *
+ * @author federico
+ */
+public class Verificacion2 {
+    //puto
+}

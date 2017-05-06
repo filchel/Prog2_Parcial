fe8a07d83c49d372977a2befb995c99d2e2c5caf
federico <federico@federico-MOV>
2017-05-06 13:55:54 -0300

    hola3

---
+++ Parcial2017/src/parcial2017/Verificacion1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial2017;
+
+/**
+ *
+ * @author federico
+ */
+public class Verificacion1 {
+    
+    private static final String usuario = "alberto";
+
+    
+    public static String getUsuario() {
+        return usuario;
+    }
+}
